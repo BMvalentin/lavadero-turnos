975a423bee46d6c2104ee589791dd8ab3654fe28
Lucho <lucianoceles@gmail.com>
2026-06-11 22:46:00 -0300

    arreglos en los toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import HomeClient from "@/components/HomeClient";
 
-void function onclick() { console.log("Booking clicked"); }
-
 export default async function HomePage() {
   return <HomeClient/>;
 }
